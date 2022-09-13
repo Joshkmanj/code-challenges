1ca7fa95d61405988738f469e889fe789a77aaf5
Josh Kralewski <joshkmanj@gmail.com>
2022-09-13 14:31:00 -0500

    encryption function created

--- javascriptChallenges/3-encryptThenDecrypt.js
+++ javascriptChallenges/3-encryptThenDecrypt.js
@@ -18,3 +18,34 @@ console.log('encryptThenDecrypt Loaded in!');
 // Together with the encryption function, you should also implement a decryption function which reverses the process.
 
 // If the string S is an empty value or the integer N is not positive, return the first argument without changes.
+
+function encrypt(text, n) {
+
+  // A variable is initiated to symbolize the separation of the values of the text and the encrypted text.
+  let encryptedText = text;
+  
+  // While loop is used to iterate the encryption logic for as many times as the n value states.
+  while (n > 0) {
+  let oddNum = '';
+  let evenNum = '';
+  
+  // This loop selects all the odd numbered text 
+  for (let i=1; i<=encryptedText.length - 1; i+=2){
+    oddNum += encryptedText[i];
+  }
+  // This loop selects all the even numbered text 
+  for (let i=0; i<=encryptedText.length - 1; i+=2){
+    evenNum += encryptedText[i];
+  }
+  
+  // The odd and even numbered text is concatenated
+  encryptedText = oddNum + evenNum
+  
+  // This increments the while condition to either the next iteration or to completion 
+  n-=1;
+  }
+  
+  // Once all iterations are complete, it returns the encrypted value
+  return encryptedText;
+  }
+  
