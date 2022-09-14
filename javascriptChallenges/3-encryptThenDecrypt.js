18395bb923172fcf4d835b89e8db5a684bbd6220
Josh Kralewski <joshkmanj@gmail.com>
2022-09-14 14:38:07 -0500

    Decryption function created with modulo and slice function to separate even and odd indexed values

--- javascriptChallenges/3-encryptThenDecrypt.js
+++ javascriptChallenges/3-encryptThenDecrypt.js
@@ -20,10 +20,8 @@ console.log('encryptThenDecrypt Loaded in!');
 // If the string S is an empty value or the integer N is not positive, return the first argument without changes.
 
 function encrypt(text, n) {
-
   // A variable is initiated to symbolize the separation of the values of the text and the encrypted text.
   let encryptedText = text;
-  
   // While loop is used to iterate the encryption logic for as many times as the n value states.
   while (n > 0) {
   let oddNum = '';
@@ -49,56 +47,73 @@ function encrypt(text, n) {
   return encryptedText;
   }
   
+
+
+
   function decrypt(encryptedText, n) {
-    let decryptedText = encryptedText;
+    let decryptedText = '';
+
+    let remainderCount = encryptedText.length % 2;
   
     // While loop is used to iterate the encryption logic for as many times as the n value states.
-   while (n > 0) {
-    let oddNum = '';
-    let evenNum = '';
+  //  while (n > 0) {
+
+    // let oddNum = '';
+    // let evenNum = '';
   
-    // This loop selects all the odd numbered text 
-    for (let i=1; i<=decryptedText.length - 1; i+=2){
-      oddNum += decryptedText[i];
-    }
-    // This loop selects all the even numbered text 
-    for (let i=0; i<=decryptedText.length - 1; i+=2){
-      evenNum += decryptedText[i];
+
+    let oddNumberCount = ((encryptedText.length - remainderCount) / 2);
+    // console.log(`remainderCount=${remainderCount}`);
+    // console.log(`oddNumberCount=${oddNumberCount}`);
+    
+    let oddNum = encryptedText.slice(0,oddNumberCount)
+    let evenNum = encryptedText.slice(0,oddNumberCount)
+    // console.log('decrypted text after slicing:', decryptedText);
+
+    for (let i = 0; i <= encryptedText.length; i++) {
+      if (i%2==0) {
+        decryptedText.push( evenNum.shift() );
+      }
+      else if (i%2==1) {
+        decryptedText.push( oddNum.shift() );
+      }
+      
     }
   
     // The odd and even numbered text is concatenated
-    encryptedText = oddNum + evenNum
     
     // This increments the while condition to either the next iteration or to completion 
-    n-=1;
-    }
+    // n-=1;
+    // }
   
     // Once all iterations are complete, it returns the encrypted value
-    return encryptedText;
+    return decryptedText;
+    // return decryptedText;
   }
   
-  console.log('begin encryption');
-  
+ 
+ 
+  // ----------------------------------------< Tests >----------------------------------------
   // console.log(`Test: "${encrypt("This is a test!", 0)}"="This is a test!"`);
   // console.log(`Test: "${encrypt("", 2)}"=""`);
-  console.log(`Test: "${encrypt("0123", 0)}"="0123"`);
-  console.log(`Test: "${encrypt("0123", 1)}"="1302"`);
-  console.log(`Test: "${encrypt("0123", 2)}"="3210"`);
-  console.log(`Test: "${encrypt("0123", 3)}"="2031"`);
-  console.log(`Test: "${encrypt("0123", 4)}"="2031"`);
-  console.log(`Test: "${encrypt("0123", 5)}"="1302"`);
-  console.log(`Test: "${encrypt("01234", 0)}"="01234"`);
-  console.log(`Test: "${encrypt("01234", 1)}"="13024"`);
-  console.log(`Test: "${encrypt("01234", 2)}"="32104"`);
-  console.log(`Test: "${encrypt("01234", 3)}"="20314"`);
-  console.log(`Test: "${encrypt("01234", 4)}"="01234"`);
-  console.log(`Test: "${encrypt("01234", 5)}"="13024"`);
-  console.log(`Test: "${encrypt("012345", 0)}"="012345"`);
-  console.log(`Test: "${encrypt("012345", 1)}"="135024"`);
-  console.log(`Test: "${encrypt("012345", 2)}"="304152"`);
-  console.log(`Test: "${encrypt("012345", 3)}"="012345"`);
-  console.log(`Test: "${encrypt("012345", 4)}"="135024"`);
-  console.log(`Test: "${encrypt("012345", 5)}"="304152"`);
+  // console.log(`Test: "${encrypt("0123", 0)}"="0123"`);
+  // console.log(`Test: "${encrypt("0123", 1)}"="1302"`);
+  // console.log(`Test: "${encrypt("0123", 2)}"="3210"`);
+  // console.log(`Test: "${encrypt("0123", 3)}"="2031"`);
+  // console.log(`Test: "${encrypt("0123", 4)}"="2031"`);
+  // console.log(`Test: "${encrypt("0123", 5)}"="1302"`);
+  // console.log(`Test: "${encrypt("01234", 0)}"="01234"`);
+  // console.log(`Test: "${encrypt("01234", 1)}"="13024"`);
+  // console.log(`Test: "${encrypt("01234", 2)}"="32104"`);
+  // console.log(`Test: "${encrypt("01234", 3)}"="20314"`);
+  // console.log(`Test: "${encrypt("01234", 4)}"="01234"`);
+  // console.log(`Test: "${encrypt("01234", 5)}"="13024"`);
+  // console.log(`Test: "${encrypt("012345", 0)}"="012345"`);
+  // console.log(`Test: "${encrypt("012345", 1)}"="135024"`);
+  // console.log(`Test: "${encrypt("012345", 2)}"="304152"`);
+  // console.log(`Test: "${encrypt("012345", 3)}"="012345"`);
+  // console.log(`Test: "${encrypt("012345", 4)}"="135024"`);
+  // console.log(`Test: "${encrypt("012345", 5)}"="304152"`);
   // console.log(`Test: "${encrypt("This is a test!", 1)}"="hsi  etTi sats!"`);
   // console.log(`Test: "${encrypt("This is a test!", 2)}"="s eT ashi tist!"`);
   // console.log(`Test: "${encrypt("This is a test!", 3)}"=" Tah itse sits!"`);
@@ -107,4 +122,10 @@ function encrypt(text, n) {
   // console.log(`Test: "${encrypt("This kata is very interesting!", 1)}"="hskt svr neetn!Ti aai eyitrsig"`);
   
   
-  // console.log('begin decryption');
+  console.log('-----------begin decryption---------');
+
+  console.log(decrypt("BDAC", 1));
+  console.log(decrypt("BDACE", 1));
+  console.log(decrypt("BDFACE", 1));
+  console.log(decrypt("BDFACEG", 1));
+  // console.log(`Test: "${decrypt("135024", 1)}"="012345"`);
